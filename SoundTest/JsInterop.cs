c0d6f1f308dcb9e08dfd54e79f2efb7108b0cf40
Michael Bond <mbond@ltisolutions.com>
2021-09-13 14:59:54 -0400

    Add project files.

---
+++ SoundTest/JsInterop.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace SoundTest
+{
+    public class JsInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        #region Constructor / Destructor
+
+        public JsInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+               "import", $"./JsInterop.js").AsTask());
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                IJSObjectReference module = await GetModule();
+                await module.DisposeAsync();
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private async Task<IJSObjectReference> GetModule() => await moduleTask.Value;
+
+        private async Task<T> InvokeAsync<T>(string method, params object[] args)
+        {
+            IJSObjectReference module = await GetModule();
+            return await module.InvokeAsync<T>(method, args);
+        }
+
+        private async Task InvokeVoidAsync(string method, params object[] args)
+        {
+            IJSObjectReference module = await GetModule();
+            await module.InvokeVoidAsync(method, args);
+        }
+
+        #endregion
+
+        public async Task GenerateSound(string type = "sine", int frequency = 440, int stopTime = 1) =>
+            await InvokeVoidAsync("generateSound", type, frequency, stopTime);
+    }
+}
---
+++ SoundTest/wwwroot/JsInterop.js
@@ -0,0 +1,12 @@
+﻿// This is a JavaScript module that is loaded on demand. It can export any number of
+// functions, and may import other JavaScript modules if required.
+
+export function generateSound(type = 'sine', frequency = 440, stopTime = 1) {
+    var context = new (window.AudioContext || window.webkitAudioContext)();
+    var osc = context.createOscillator(); // instantiate an oscillator
+    osc.type = type ?? 'sine'; // this is the default - also square, sawtooth, triangle
+    osc.frequency.value = frequency ?? 440; // Hz
+    osc.connect(context.destination); // connect it to the destination
+    osc.start(); // start the oscillator
+    osc.stop(context.currentTime + stopTime ?? 1); // stop 2 seconds after the current time
+}
